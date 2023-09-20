d9a6fd5cddebed9ea83d1bc9a31a644097d42142
rlaig <raymundolaig@gmail.com>
2023-09-20 13:37:38 +0800

    feat: campaigns api

--- pages/api/campaigns.ts
+++ pages/api/campaigns.ts
@@ -1,26 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import campaignsData from './campaignsResponse.json'
 import { isNotNil } from 'ramda'
-import {CampaignData, responseData} from '../types'
+import { CampaignData, responseData } from '../types'
 
 const getCampaigns = (
   req: NextApiRequest,
   res: NextApiResponse<responseData<CampaignData[]>>
 ) => {
-  const search = isNotNil(req.query.search) && !Array.isArray(req.query.search) &&req.query.search || undefined
+  const search =
+    (isNotNil(req.query.search) &&
+      !Array.isArray(req.query.search) &&
+      req.query.search.toLowerCase()) ||
+    undefined
+
   const current_page = Number(req.query.page) || 1
   const page_size = Number(req.query.size) || 4
 
   // filter
-  const filteredData = isNotNil(search) ? 
-    campaignsData.filter(
-      (campaign: CampaignData) =>
-        campaign.name.toLowerCase().indexOf(search) >=0 || campaign.tags.filter(v => v.toLowerCase().indexOf(search) >=0).length
-    )
-  : campaignsData
+  const filteredData = isNotNil(search)
+    ? campaignsData.filter(
+        (campaign: CampaignData) =>
+          campaign.name.toLowerCase().indexOf(search) >= 0 ||
+          campaign.tags.filter((v) => v.toLowerCase().indexOf(search) >= 0)
+            .length
+      )
+    : campaignsData
 
   // pagination
-  const startI = (current_page-1) * page_size
+  const startI = (current_page - 1) * page_size
   const endI = startI + page_size
   const paginatedData = filteredData.slice(startI, endI)
 
@@ -31,19 +38,18 @@ const getCampaigns = (
       total: filteredData.length,
       last_page: Math.ceil(filteredData.length / page_size),
       current_page,
-      page_size
-    }
+      page_size,
+    },
   }
+
   return res.status(200).json(response)
 }
- 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  switch(req.method) {
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
     case 'GET':
       getCampaigns(req, res)
+      break
     case 'POST':
     case 'PATCH':
     case 'DELETE':
